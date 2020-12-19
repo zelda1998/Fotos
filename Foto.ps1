b873ab4ae5268f17f8feec4ef11f7eb73f5c7f1b
FLira2000 <fabioliradev@gmail.com>
2020-12-19 15:27:23 -0300

    major configuration

--- Foto.ps1
+++ Foto.ps1
@@ -11,7 +11,7 @@ function Test-Or-Create-Dir()
    }
    else {
       New-Item $folderName -ItemType "Directory" | Out-Null
-      return 0
+      return 1
    }  
 
 }
@@ -28,17 +28,25 @@ function Get-Host-Ip()
    }
 }
 
-function Write-Log()
+function Write-Log($status)
 {
    $dia = Get-Date -Format 'dd/MM/yyyy'
    $hora = Get-Date -Format 'hh:mm'
    $ip = Get-Host-Ip
 
-   Write-Output $dia'; '$hora'; '$ip | Out-File -FilePath $logName -Encoding utf8 -Append
+   if($status -eq 1)
+   {
+      Write-Output "$dia'; '$hora'; '$ip; Successful"  | Out-File -FilePath $logName -Encoding utf8 -Append
+   }
+   else 
+   {
+      Write-Output "$dia'; '$hora'; '$ip; Failed" | Out-File -FilePath $logName -Encoding utf8 -Append
+   }
 }
 
-function Get-Images($photo_name) 
+function Get-Image($photo_name) 
 {
+   $status = 1
    try
    {
       Invoke-WebRequest $url -OutFile "$photo_name.jpeg" | Out-Null
@@ -46,32 +54,57 @@ function Get-Images($photo_name)
    catch
    {
       Write-Host "Ocorreu um erro ao realizar o download da imagem."
-   }
-}
-
-<#
-## EXECUTION
-do 
-{
-   if ($(Test-Or-Create-Dir) -eq 0) 
-   {
-      Write-Host "Já existe um diretório cujo nome é utilizado pelo processo. Deseja utilizá-lo?" -ForegroundColor Red
-      Write-Host "[S/N]: " -NoNewline
-      $asw = Read-Host 
-      if ($asw -eq 'S' -or $asw -eq 'N') {
-         break
-      }
-      else 
-      {
-         Write-Host "Opcão Inválida."   
-      }
+      $status = 0
    }
    
-} until (0>1)
-
-for ($i = 0; $i -lt $qtdImgs; $i++)
-{
-   Get-Images("photo$i")
+   return $status
 }
-#>
+
+function Invoke-Get-Image($ammount) 
+{
+   Set-Location $folderName
+
+   for ($i = 0; $i -lt $ammount; $i++) 
+   {
+      Get-Image("photo$i")
+   }
+}
+
+function Manage{
+   do 
+   {
+      if ($(Test-Or-Create-Dir) -eq 0) 
+      {
+         Write-Host "Já existe um diretório cujo nome é utilizado pelo processo. Deseja utilizá-lo?" -ForegroundColor Red
+         Write-Host "[S/N]: " -NoNewline
+         $asw = Read-Host 
+         if ($asw -eq 'S' -or $asw -eq 'N') 
+         {
+            return $asw
+         }
+         else 
+         {
+            Write-Host "Opcão Inválida."   
+         }
+      }
+      else
+      {
+         Write-Host "Iniciando downloads... " -ForegroundColor Yellow
+         return 'S'
+      }
+   } while(1)
+}
+
+#EXECUTION
+
+if ($(Manage) -eq 'S'){
+   Write-Log( $( Invoke-Get-Image( $qtdImgs ) ) )
+   Set-Location '..'
+}
+else 
+{
+   Write-Host "Terminando o processo..."
+   Exit-PSHostProcess 1
+}
+
 
